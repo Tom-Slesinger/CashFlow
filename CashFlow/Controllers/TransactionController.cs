646946b7890b5cff47ebba2524fee5e714122929
Tom Slesinger <tom.slesinger@gmail.com>
2024-08-14 16:52:24 -0600

    Finished CRUD of transactions using JS / ASP NET

--- CashFlow/Controllers/TransactionController.cs
+++ CashFlow/Controllers/TransactionController.cs
@@ -1,5 +1,6 @@
 ﻿using CashFlow.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
 
 namespace CashFlow.Controllers
 {
@@ -10,6 +11,7 @@ namespace CashFlow.Controllers
         {
             _context = context;
         }
+
         public IActionResult Index()
         {
 
@@ -25,23 +27,60 @@ namespace CashFlow.Controllers
 
         [HttpPost]
         [Route("Transaction/SaveTransaction")]
-        public IActionResult SaveTransaction([FromBody] Transaction transaction)
+        public IActionResult SaveTransaction([FromBody] Transaction transactionToSave)
         {
-            if (transaction.Amount == null)
+            if (transactionToSave.Amount == null)
             {
                 return BadRequest("Amount is a required field.");
             }
 
-            if (transaction.Description == null)
+            if (transactionToSave.Description == null)
             {
                 return BadRequest("Descrption is a required field.");
             }
 
-            _context.Transactions.Add(transaction);
+            if (transactionToSave.ID == 0)
+            {
+                _context.Transactions.Add(transactionToSave);
+            }
+            else
+            {
+                Transaction? existingTransaction = _context.Transactions.FirstOrDefault(x => x.ID == transactionToSave.ID);
+                if (existingTransaction == null)
+                {
+                    return BadRequest("Cannot find Transaction with that ID in the Database. Please contact your System Administrator.");
+                }
+
+                existingTransaction.Amount = transactionToSave.Amount;
+                existingTransaction.Description = transactionToSave.Description;
+                existingTransaction.TransactionType = transactionToSave.TransactionType;
+            }
             _context.SaveChanges();
             return Ok();
         }
+        public class DeleteTransactionModel
+        {
+            public int? ID { get; set; }
+        }
+        [HttpPost]
+        [Route("Transaction/DeleteTransaction")]
+        public IActionResult DeleteTransaction([FromBody] DeleteTransactionModel? obj)
+        {
+            if (obj == null || obj.ID == null || obj.ID == 0)
+            {
+                return BadRequest("ID Cannot be Null or 0.");
+            }
+            Transaction? transactionToDelete = _context.Transactions.FirstOrDefault(x => x.ID == obj.ID);
+            if (transactionToDelete == null)
+            {
+                return BadRequest("Could not find a record to Delete.");
+            }
 
+            _context.Remove(transactionToDelete);
+            _context.SaveChanges();
+
+            return Ok();
+        }
         public List<Transaction> GetTransactions()
         {
             var transactions = _context.Transactions.ToList();
--- CashFlow/wwwroot/js/transaction.js
+++ CashFlow/wwwroot/js/transaction.js
@@ -1,37 +1,82 @@
 ﻿window.addEventListener('load', function () {
-    //Bind Lisnters
-    BindTransactionSubmit();
+    BindTransactionCRUDControls();
+
 });
 
 
-function Bind() {
-
+function BindTransactionCRUDControls() {
+    BindTransactionSave();
+    BindTransactionEdit();
+    BindTransactionDelete();
+    BindTransactionCreate();
+    BindTransactionClose();
 }
 
-function BindTransactionSubmit() {
+function BindTransactionSave() {
     let submitBtn = $('#addTransaction')[0];
-    console.log(submitBtn);
     submitBtn.addEventListener('click', function () {
         PostTransaction();
     })
 }
+
+function BindTransactionEdit() {
+    let editBtnList = document.querySelectorAll("[id='editTransaction']");
+    for (let i = 0; i < editBtnList.length; i++) {
+        editBtnList[i].addEventListener('click', function () {
+            EditTransaction(editBtnList[i].getAttribute('data-custom-id'));
+        });
+    }
+}
+
+function BindTransactionDelete() {
+    let deleteBtnList = document.querySelectorAll("[id='deleteTransaction']");
+    for (let i = 0; i < deleteBtnList.length; i++) {
+        deleteBtnList[i].addEventListener('click', function () {
+            DeleteTransaction(deleteBtnList[i].getAttribute('data-custom-id'));
+        });
+    }
+}
+
+function BindTransactionEdit() {
+    let editBtnList = document.querySelectorAll("[id='deleteTransaction']");
+    for (let i = 0; i < editBtnList.length; i++) {
+        editBtnList[i].addEventListener('click', function () {
+            EditTransaction(editBtnList[i].getAttribute('data-custom-id'));
+        });
+    }
+}
+
+function BindTransactionCreate() {
+    let createBtn = $('#createTransaction')[0];
+    createBtn.addEventListener('click', function () {
+        $('#exampleModal').modal('show');
+    });
+}
+
+function BindTransactionClose() {
+    let closeBtn = $('#closeTransaction')[0];
+    closeBtn.addEventListener('click', function () {
+        $('#exampleModal').modal('hide');
+    })
+}
+
 
 
 function PostTransaction() {
     let description = document.querySelector("[name=description]").value;
     let amount = document.querySelector("[name=amount]").value;
     let type = document.querySelector("[name=transactiontype]").value;
+    let id = document.querySelector("[name=id]").value;
     if (description == null || amount == null || type == null) {
         window.alert('You must enter all data.');
         return;
     }
 
     const transactionData = {
-        ID: 0, Description: description, Amount: amount,
+        ID: id == null ? 0 : id, Description: description, Amount: amount,
         TransactionType: type === "Expense" ? 0 : 1 // 0 for Expense, 1 for Income
     };
-    console.log(transactionData);
-    let success = true;
+
     $.ajax({
         async: false,
         type: "Post",
@@ -40,16 +85,43 @@ function PostTransaction() {
         contentType: "application/json",
         dataType: 'html',
         success: function (response) {
-            window.alert('Data Saved!');
             $('#exampleModal').modal('hide');
+            location.reload();
         },
         error: function (request, textStatus, errorThrown) {
             success = false;
             console.error(request.responseText, request.status);
         }
     });
+}
 
-    if (success === true) {
-        $('#exampleModal').modal('hide');
-    }
+function DeleteTransaction(dataID) {
+    const dataToPost = { ID: dataID };
+    $.ajax({
+        async: false,
+        type: "Post",
+        url: '/Transaction/DeleteTransaction',
+        data: JSON.stringify(dataToPost),
+        contentType: "application/json",
+        dataType: 'html',
+        success: function () {
+            location.reload();
+        },
+        error: function (request, textStatus, errorThrown) {
+            success = false;
+            console.error(request.responseText, request.status);
+        }
+    });
+}
+
+function EditTransaction(dataID) {
+    setModalValues(dataID);
+    $('#exampleModal').modal('show');
+}
+
+function setModalValues(dataID) {
+    document.querySelector("[name=description]").value = $('#' + `${dataID}desc`)[0].textContent.trim();
+    document.querySelector("[name=amount]").value = $('#' + `${dataID}amount`)[0].textContent.trim();
+    document.querySelector("[name=transactiontype]").value = $('#' + `${dataID}tt`)[0].textContent.trim();
+    document.querySelector("[name=id]").value = dataID;
 }
